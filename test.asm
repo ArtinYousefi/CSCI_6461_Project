c6662b67c37c1b957617ad772233630c2b97bf32
Madi252 <144497458+Madi252@users.noreply.github.com>
2025-02-08 21:10:31 -0500

    Update test.asm
    Updating to new test

--- test.asm
+++ test.asm
@@ -1,10 +1,19 @@
-LOC 10           ; Start at memory location 10
-Data 5          ; Store 5 at location 10
-Data 8          ; Store 8 at location 11
-Data 0          ; Placeholder for computation result
-LDX 1,11        ; Load index register X1 with value at location 11 (which is 8)
-LDR 2,0,10      ; Load R2 with contents of memory location 10 (which is 5)
-LDR 3,1,10      ; Load R3 with contents of memory location (10 + X1)
-ADD 2,3,0       ; Add R2 and R3, store in R2
-STR 2,0,12      ; Store result at memory location 12
-HLT             ; Halt execution
+LOC 4           ; Begin at memory location 4
+Data 22         ; Store 10 at location 5
+Data 3          ; Store 3 at location 6
+Data 73         ; Store 12 at location 7
+Data 9         ; Store 12 at location 8
+Data End        ; Store address of "End" at location 9
+Data 9
+Data 0
+LDX 1,6        ; Load index register X1 with value at location 6 (which is 3)
+LDR 2,0,7     ; Load R2 with contents of memory location 7
+LDR 3,2,8     ; Load R3 with contents of memory location 8
+LDR 1,3,8     ; Load R3 with contents of memory location 8
+LDX 3,5        ; Load X3 with contents of location 5 
+LDR 1,2,7,1   ; Load R1 with contents of memory location (EA computed with indirect addressing)
+LDA 0,0,0      ; Load register R0 with address 0
+LDX 2,9        ; Load X2 with contents of location 9 (which is the address of "End")
+JZ 0,1,0       ; Jump to "End" if R0 == 0
+LOC 1024
+End: HLT       ; Stop execution
