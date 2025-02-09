8df2966b0dccfc840db20f9f2ada734b9530a2fc
ArtinYousefi <65470249+ArtinYousefi@users.noreply.github.com>
2025-02-08 20:52:29 -0500

    Update test.asm

--- test.asm
+++ test.asm
@@ -2,7 +2,6 @@ LOC 10           ; Start at memory location 10
 Data 5          ; Store 5 at location 10
 Data 8          ; Store 8 at location 11
 Data 0          ; Placeholder for computation result
-
 LDX 1,11        ; Load index register X1 with value at location 11 (which is 8)
 LDR 2,0,10      ; Load R2 with contents of memory location 10 (which is 5)
 LDR 3,1,10      ; Load R3 with contents of memory location (10 + X1)
