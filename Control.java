97424f6cdbe3bdff6f98898fcae3fffa4060a3d2
Madi252 <144497458+Madi252@users.noreply.github.com>
2025-03-01 20:53:43 -0500

    Update Control.java
    loop and updates now working need to clean up and connect with GUI for the most part

--- Control.java
+++ Control.java
@@ -1,7 +1,7 @@
-package tryingtowork;
 import java.io.*;
 import java.util.*;
 
+//note all shorts have been swapped to ints and will implement a check so that they don't go over the 16 bit size of 65,535
 public class Control {
 
 	public static Memory mem = new Memory();
@@ -36,17 +36,17 @@ public class Control {
     
     
     //function to compute effective address - values are decimal
-    public static short computeEA (byte ix, byte addr, byte indirect) {
-    	short res = 0;    	
+    public static int computeEA (byte ix, byte addr, byte indirect) {
+    	int res = 0;    	
     	
     	if(ix == 0) {
-    		res = mem.readWord(addr);
+    		res = addr;
     	}else if (ix == 1 || ix == 2 || ix == 3) {
-    		res = (short) (ix + mem.readWord(addr));
+    		res = (ix + addr); //was mem.readWord(addr) but that was incorrect - misread doc
     	}
     	
     	if(indirect ==  1) {
-    		short tmp = mem.readWord(res);
+    		int tmp = mem.readWord(res);
     		res = tmp;
     	}
     	return res;
@@ -71,15 +71,15 @@ public class Control {
             String addrStr = octalToBinary(parts[0]);
             String instStr = octalToBinary(parts[1]);
            
-            short addr = Short.parseShort(String.valueOf(addrStr), 2);
-            short inst = Short.parseShort(String.valueOf(instStr), 2);
+            int addr = Integer.parseInt(String.valueOf(addrStr), 2); //storing as a decimal
+            int inst = Integer.parseInt(String.valueOf(instStr), 2);
+
             
             if (counter == 0) { //at first line, init the pc
             	mem.PC = addr;
             }
             
             mem.writeWord(addr,inst);  //store instructions at address in memory
-            
             counter++;
 //            System.out.println(String.format("%16s",Long.toBinaryString(mem.readWord(addr))).replace(" ", "0"));
         }
@@ -89,38 +89,26 @@ public class Control {
     //Load register from memory
 	public static void ldr(byte r, byte ix, byte addr, byte indirect) {
 		//short ea = computeEA(ix,addr,indirect);
-		System.out.println(mem.GPR[r]);	
+		//System.out.println(mem.GPR[r]);	
 		mem.GPR[r] = mem.readWord(mem.MAR); //might have to use MBR and stuff for this!
-		System.out.println(mem.GPR[r]);	
+		//System.out.println(mem.GPR[r]);	
 	}
 	
 	//store register to memory
 	public static void str(byte r, byte ix, byte addr, byte indirect) {
-		//short ea = computeEA(ix,addr,indirect);
-		System.out.println(mem.readWord(mem.MAR));
 		mem.writeWord(mem.MAR, mem.GPR[r]);
-		System.out.println(mem.readWord(mem.MAR));
 	}
 	//load register with address
-	public static void lda(byte r, byte ix, byte addr, byte indirect) {
-		//short ea = computeEA(ix,addr,indirect);
-		System.out.println(mem.GPR[r]);	
+	public static void lda(byte r, byte ix, byte addr, byte indirect) {	
 		mem.GPR[r] = mem.MAR;
-		System.out.println(mem.GPR[r]);	
 	}
 	//load index register from memory
 	public static void ldx(byte ix, byte addr, byte indirect) {		//when loading and storing need -1 due to arrays starting at 0
-		//short ea = computeEA(ix,addr,indirect);
-		System.out.println(mem.IX[ix-1]);	
 		mem.IX[ix-1] = mem.readWord(mem.MAR);
-		System.out.println(mem.IX[ix-1]);	
 	}
 	//store index register to memory
 	public static void stx(byte ix, byte addr, byte indirect) {
-		//short ea = computeEA(ix,addr,indirect);
-		System.out.println(mem.readWord(mem.MAR));
 		mem.writeWord(mem.MAR, mem.IX[ix-1]);
-		System.out.println(mem.readWord(mem.MAR));
 	}
     
 	//runs the simulator, populates the registers and executes the instructions
@@ -140,19 +128,20 @@ public class Control {
 			
 			//SO rn breaks b/c data has a 000000 operand need to swap the HLT check to be here (and check if the entire instruction is 0)
 			//then below if the operand is 000000 store data at that address
-			if (binary == "0000000000000000") { //HLT need to figure out better way later
-	        	mem.PC = 0;
+			if (binary.equals("0000000000000000")) { //HLT need to figure out better way later
+	        	System.out.println("HLT found"); //not quite accurate but needed for now
+				mem.PC = 0;
 	        	return; //need to properly finish but for now just force an end
 			}
 			//operand
 	        String op = binary.substring(0,6);
 	        Byte op_val = Byte.parseByte(String.valueOf(op), 2);
-	        //index (IX)
-	        String ix = binary.substring(6,8);
-	        Byte ix_val = Byte.parseByte(String.valueOf(ix), 2);
 	        //register (GPR)
-	        String r = binary.substring(8,10);
+	        String r = binary.substring(6,8);
 	        Byte r_val = Byte.parseByte(String.valueOf(r), 2);
+	        //index (IX)
+	        String ix = binary.substring(8,10);
+	        Byte ix_val = Byte.parseByte(String.valueOf(ix), 2);
 	        //indirect
 	        String i = binary.substring(10,11);
 	        Byte i_val = Byte.parseByte(String.valueOf(i), 2);
@@ -161,49 +150,28 @@ public class Control {
 	        Byte addr_val = Byte.parseByte(String.valueOf(addr), 2);
 	        
 			//get instruction, convert to binary, set all vars, do case to determine which function to call
-	        short ea = computeEA(ix_val,addr_val,i_val);
+	        int ea = computeEA(ix_val,addr_val,i_val);
 	        mem.MAR = ea; //updating MAR to be effective address
 	        
-	        System.out.println("Before");
-	       
-	        System.out.println(op);
-	        
-	        System.out.println(Arrays.toString(mem.IX));
-	        System.out.println(Arrays.toString(mem.GPR));
-	        
-	        switch (op) {
-		        case "000001":
-		        	ldr(r_val, ix_val, addr_val, i_val);
-		        	break;
-		        case "000010":
-		        	str(r_val, ix_val, addr_val, i_val);
-		        	break;
-		        case "000011":
-		        	lda(r_val, ix_val, addr_val, i_val);
-		        	break;
-		        case "100001":
-		        	ldx(ix_val, addr_val, i_val);
-		        	break;
-		        case "100010":
-		        	stx(ix_val, addr_val, i_val);
-		        	break;
-		        case "000000":
-		        	//store data at address
-		        	mem.writeWord(mem.PC, addr_val);
-		        	
-		        default:
-		        	System.out.println("operand not recognized");        
+	        //swaped from case to use equals instead
+	        if(op.equals("000001")) {
+	        	ldr(r_val, ix_val, addr_val, i_val);
+	        }else if (op.equals("000010")) {
+	        	str(r_val, ix_val, addr_val, i_val);
+	        }else if (op.equals("000011")) {
+	        	lda(r_val, ix_val, addr_val, i_val);
+	        }else if (op.equals("100001")) {
+	        	ldx(ix_val, addr_val, i_val);
+	        }else if (op.equals("100010")) {
+	        	stx(ix_val, addr_val, i_val);
+	        }else if (op.equals("000000")) { //DONT NEED THIS THIS HAS ALREADY BEEN DONE mem.writeWord(mem.PC, addr_val);
+	        	System.out.println("data already loaded");
+	        }else {
+	        	System.out.println("operand not recognized");
 	        }
-	        
-	        System.out.println("After");
-	        
-	        System.out.println(Arrays.toString(mem.IX));
-	        System.out.println(Arrays.toString(mem.GPR));
-	        
+
 	        mem.PC++;
-	     
-			//DURING this set all registers properly
-			//make this a do while mem.readword(address) != 0, if it = 0 exit (act as a HLT) 
+
 		}
 		System.out.println("file finished"); 
 	}
@@ -212,16 +180,15 @@ public class Control {
         // Create a new memory instance
         Memory mem = new Memory();
         
-        
+        //the mem and loadLF can be called by the GUI when the file is "selected"/init is selected
         String file = "src\\load4.txt"; //will swap to args[0] once it works, for others 
         try {
 			loadLF(file); 
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
         
-        runSimulator();
+        runSimulator();  //should run when run is selected in GUI
 
         
     }
